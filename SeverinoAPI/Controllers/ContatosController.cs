819a13b841982005eb3964967e12db9cf69fd039
Jéssica Campos <jessica.campos@engecompany.com.br>
2019-08-13 16:13:54 -0300

    initial

---
+++ SeverinoAPI/Controllers/ContatosController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using SeverinoAPI.Models;
+
+namespace SeverinoAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ContatosController : ControllerBase
+  {
+
+    public Contato _contatoAnterior = new Contato
+    {
+      Nome = "Jim",
+      Email = "jim1@gmail.com",
+      Id = new Guid()
+    };
+
+
+    // GET api/values
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    /// <summary>
+    /// Segue abaixo o Json para testar essa Action
+    /// 
+    /// [{ op: "replace",  "path": "/Email", value: "jessica.campos@live.com" },{ op: "replace",  "path": "/Nome", value: "Jessica Campos" }]
+    /// 
+    /// </summary>
+    /// <param name="novoContato"></param>
+    /// <returns></returns>
+    // PATCH api/values/5
+    [HttpPatch]
+    public IActionResult Patch([FromBody] JsonPatchDocument<Contato> novoContato)
+    {
+      if (novoContato != null)
+      {        
+
+        novoContato.ApplyTo(_contatoAnterior, ModelState);
+
+        if (!ModelState.IsValid)
+        {
+          return BadRequest(ModelState);
+        }
+
+        return new ObjectResult(_contatoAnterior);
+      }
+      else
+      {
+        return BadRequest(ModelState);
+      }
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
---
+++ SeverinoAPI/Models/Contato.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeverinoAPI.Models
+{
+  public class Contato
+  {
+    public Contato()
+    {
+
+    }
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+  }
+
+
+  public class ContatoDao
+  {
+    
+    public List<Contato> getAllContatos()
+    {
+      return new List<Contato>() {
+      new Contato(){Email = "123@.com.br", Id = new Guid(), Nome = "Sr 123"},
+      new Contato(){Email = "456@.com.br", Id = new Guid(), Nome = "Sr 456"},
+      new Contato(){Email = "789@.com.br", Id = new Guid(), Nome = "Sr 789"},
+      new Contato(){Email = "147@.com.br", Id = new Guid(), Nome = "Sr 147"},
+      new Contato(){Email = "258@.com.br", Id = new Guid(), Nome = "Sr 258"},
+      new Contato(){Email = "369@.com.br", Id = new Guid(), Nome = "Sr 369"},
+      new Contato(){Email = "852@.com.br", Id = new Guid(), Nome = "Sr 852"},
+      new Contato(){Email = "741@.com.br", Id = new Guid(), Nome = "Sr 741"},
+      new Contato(){Email = "963@.com.br", Id = new Guid(), Nome = "Sr 963"}
+      };
+    }
+  }
+}
